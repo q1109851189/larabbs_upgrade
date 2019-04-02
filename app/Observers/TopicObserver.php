7a75f7dc96a4669e41c2e4c3eca75d5dc3edce7d
冯晓宇 <18645693593@163.com>
2019-04-02 08:10:37 +0800

    翻译slug使用队列

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,14 +3,14 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class TopicObserver
 {
-    public function saving(Topic $topic)
+    public function saved(Topic $topic)
     {
         // XSS 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
@@ -20,7 +20,8 @@ class TopicObserver
 
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
