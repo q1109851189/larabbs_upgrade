b5ba0719f12072d709c37e105b8923b9e71bf829
冯晓宇 <18645693593@163.com>
2019-04-03 11:45:17 +0800

    添加管理员权限和horizen权限

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -43,5 +43,12 @@ class UsersTableSeeder extends Seeder
         $user->email = 'Pest_Rose@learnku.com';
         $user->avatar = 'http://larabbs_upgrade.test/uploads/images/avatars/201903/26/1_1553593955_Baz4bxj1Cz.jpg';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
     }
 }
