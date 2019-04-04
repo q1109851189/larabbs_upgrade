fb57a2b13f78165a1ebee14574c4d63662d2bc76
冯晓宇 <18645693593@163.com>
2019-04-04 18:30:31 +0800

    管理员权限
    Signed-off-by: 冯晓宇 <18645693593@163.com>

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,5 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 消息通知
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
