cc4ab896da4e3fde8136cfe39b19d38f47865e7d
冯晓宇 <18645693593@163.com>
2019-04-25 18:57:51 +0800

    话题回复列表

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,14 @@ $api->version('v1', [
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
 
+        // 话题回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
+
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
